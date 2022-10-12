eaeebcc9f5bf73119aa36f3740e4ab30a43270ed
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2022-10-12 12:26:33 +0100

    removed console.logs

--- index.js
+++ index.js
@@ -74,10 +74,8 @@ function displayFutureForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayFutureForecast);
 }
 
@@ -132,7 +130,6 @@ function searchMyLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(weatherCurrent);
-  console.log(apiUrl);
 }
 
 function getMyLocation(event) {
